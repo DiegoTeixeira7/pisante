3a4f2700008a5e201dce5125b7439ad13abc5175
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2022-02-21 17:39:48 -0300

    added component product

---
+++ components/Product.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+
+    <v-img
+      height="250"
+      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+    ></v-img>
+
+    <!-- <v-card-title>{{ item.title ? item.title : '' }}</v-card-title> -->
+
+    <v-card-text>
+      <div class="my-2 text-subtitle-1">
+        <!-- R${{ item.price ? item.price : '0,00' }} -->
+      </div>
+
+      <div class="tw-mt-2">
+        Small plates, salads & sandwiches - an intimate setting with 12 indoor
+        seats plus patio seating.
+      </div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+
+    <v-card-actions>
+      <v-btn color="deep-purple lighten-2" text> Adicionar no carrinho </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => ({}),
+  methods: {},
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="tw-mt-8 tw-text-3xl tw-font-bold tw-underline">Hello world!</h1>
+    <product />
   </div>
 </template>
 
