66a716c6c64ae4aeff264cce8367e03b2018ab24
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2022-02-26 09:27:38 -0300

    list products

--- components/Product.vue
+++ components/Product.vue
@@ -1,37 +1,84 @@
 <template>
-  <v-card :loading="loading" class="mx-auto my-12" max-width="374">
-    <template slot="progress">
-      <v-progress-linear
-        color="deep-purple"
-        height="10"
-        indeterminate
-      ></v-progress-linear>
-    </template>
+  <nuxt-link :to="item.slug">
+    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+      <template slot="progress">
+        <v-progress-linear
+          color="deep-purple"
+          height="10"
+          indeterminate
+        ></v-progress-linear>
+      </template>
 
-    <v-img
-      height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-    ></v-img>
+      <v-img
+        height="250"
+        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+      ></v-img>
 
-    <!-- <v-card-title>{{ item.title ? item.title : '' }}</v-card-title> -->
+      <v-card-title>{{ item.title ? item.title : '' }}</v-card-title>
 
-    <v-card-text>
-      <div class="my-2 text-subtitle-1">
-        <!-- R${{ item.price ? item.price : '0,00' }} -->
-      </div>
+      <v-card-text>
+        <div class="my-2 text-subtitle-1">
+          R${{ item.price ? item.price : '0,00' }}
+        </div>
 
-      <div class="tw-mt-2">
-        Small plates, salads & sandwiches - an intimate setting with 12 indoor
-        seats plus patio seating.
-      </div>
-    </v-card-text>
+        <div class="tw-mt-2">
+          <span
+            ><b>Marca:</b>
+            {{ item.model && item.model.brand ? item.model.brand : '' }}</span
+          >
+          <br />
+          <span
+            ><b>Material:</b>
+            {{
+              item.model && item.model.material ? item.model.material : ''
+            }}</span
+          >
+          <br />
+          <span
+            ><b>Público:</b>
+            {{
+              item.model && item.model.intendedAudience
+                ? item.model.intendedAudience
+                : ''
+            }}</span
+          >
+          <br />
+          <span
+            ><b>Fechamento:</b>
+            {{
+              item.model && item.model.closure ? item.model.closure : ''
+            }}</span
+          >
+          <br />
+          <span
+            ><b>Amortecedores:</b>
+            {{
+              item.model && item.model.isShockAbsorbers
+                ? 'Possui'
+                : 'Não possui'
+            }}</span
+          >
+          <br />
+          <span
+            ><b>Palmilha anti-odor:</b>
+            {{
+              item.model && item.model.isAntiOdourInsole
+                ? 'Possui'
+                : 'Não possui'
+            }}</span
+          >
+        </div>
+      </v-card-text>
 
-    <v-divider class="mx-4"></v-divider>
+      <v-divider class="mx-4"></v-divider>
 
-    <v-card-actions>
-      <v-btn color="deep-purple lighten-2" text> Adicionar no carrinho </v-btn>
-    </v-card-actions>
-  </v-card>
+      <!-- <v-card-actions>
+        <v-btn color="deep-purple lighten-2" text>
+          Adicionar no carrinho
+        </v-btn>
+      </v-card-actions> -->
+    </v-card>
+  </nuxt-link>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,194 @@
 <template>
-  <div>
-    <product />
+  <div class="tw-m-8">
+    <div
+      class="
+        tw-mt-4 tw-w-full tw-grid tw-grid-cols-1
+        lg:tw-grid-cols-2
+        tw-justify-items-center tw-gap-2
+      "
+    >
+      <div v-for="(item, index) in items" :key="index">
+        <product v-if="item.page === page" :item="item" />
+      </div>
+    </div>
+    <div class="text-center tw-mt-8">
+      <v-pagination v-model="page" :length="numberPages" circle></v-pagination>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    page: 1,
+    items: [
+      {
+        title: 'Produto 1',
+        slug: '/produto-1',
+        price: 122.33,
+        availableQuantity: 10,
+        page: 1,
+        model: {
+          brand: 'Nike',
+          material: 'Couro',
+          intendedAudience: 'Masculino',
+          closure: 'Cadarço',
+          isShockAbsorbers: true,
+          isAntiOdourInsole: true,
+          inventory: [
+            {
+              quantity: 4,
+              size: 38,
+              color: 'Azul',
+            },
+            {
+              quantity: 4,
+              size: 39,
+              color: 'Preto',
+            },
+            {
+              quantity: 2,
+              size: 40,
+              color: 'Branco',
+            },
+          ],
+        },
+      },
+      {
+        title: 'Produto 2',
+        slug: '/produto-2',
+        price: 162.33,
+        availableQuantity: 10,
+        page: 1,
+        model: {
+          brand: 'Nike',
+          material: 'Couro',
+          intendedAudience: 'Masculino',
+          closure: 'Cadarço',
+          isShockAbsorbers: true,
+          isAntiOdourInsole: true,
+          inventory: [
+            {
+              quantity: 4,
+              size: 38,
+              color: 'Azul',
+            },
+            {
+              quantity: 4,
+              size: 39,
+              color: 'Preto',
+            },
+            {
+              quantity: 2,
+              size: 40,
+              color: 'Branco',
+            },
+          ],
+        },
+      },
+      {
+        title: 'Produto 3',
+        slug: '/produto-3',
+        price: 112.13,
+        availableQuantity: 10,
+        page: 1,
+        model: {
+          brand: 'Nike',
+          material: 'Couro',
+          intendedAudience: 'Masculino',
+          closure: 'Cadarço',
+          isShockAbsorbers: true,
+          isAntiOdourInsole: true,
+          inventory: [
+            {
+              quantity: 4,
+              size: 38,
+              color: 'Azul',
+            },
+            {
+              quantity: 4,
+              size: 39,
+              color: 'Preto',
+            },
+            {
+              quantity: 2,
+              size: 40,
+              color: 'Branco',
+            },
+          ],
+        },
+      },
+      {
+        title: 'Produto 4',
+        slug: '/produto-4',
+        price: 195.98,
+        availableQuantity: 10,
+        page: 1,
+        model: {
+          brand: 'Nike',
+          material: 'Couro',
+          intendedAudience: 'Masculino',
+          closure: 'Cadarço',
+          isShockAbsorbers: true,
+          isAntiOdourInsole: true,
+          inventory: [
+            {
+              quantity: 4,
+              size: 38,
+              color: 'Azul',
+            },
+            {
+              quantity: 4,
+              size: 39,
+              color: 'Preto',
+            },
+            {
+              quantity: 2,
+              size: 40,
+              color: 'Branco',
+            },
+          ],
+        },
+      },
+      {
+        title: 'Produto 5',
+        slug: '/produto-5',
+        price: 195.98,
+        availableQuantity: 10,
+        page: 2,
+        model: {
+          brand: 'Nike',
+          material: 'Couro',
+          intendedAudience: 'Masculino',
+          closure: 'Cadarço',
+          isShockAbsorbers: true,
+          isAntiOdourInsole: true,
+          inventory: [
+            {
+              quantity: 4,
+              size: 38,
+              color: 'Azul',
+            },
+            {
+              quantity: 4,
+              size: 39,
+              color: 'Preto',
+            },
+            {
+              quantity: 2,
+              size: 40,
+              color: 'Branco',
+            },
+          ],
+        },
+      },
+    ],
+  }),
+  computed: {
+    numberPages() {
+      const number = Math.ceil(this.items.length / 4)
+      return number
+    },
+  },
+}
 </script>
