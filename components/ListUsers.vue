50ab28ba5252d00baca5edf79bb88d2336fb57bc
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2022-03-27 15:28:16 -0300

    listando todos usuários

---
+++ components/ListUsers.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-card class="mx-auto" max-width="400" outlined>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-subtitle>
+          <span><b>Rua:</b> {{ user.street ? user.street : '' }}</span>
+          <br />
+          <span><b>Número:</b> {{ user.number ? user.number : '' }}</span>
+          <br />
+          <span><b>Cidade:</b> {{ user.city ? user.city : '' }}</span>
+          <br />
+          <span><b>Estado:</b> {{ user.state ? user.state : '' }}</span>
+          <br />
+          <span><b>País:</b> {{ user.country ? user.country : '' }}</span>
+          <br />
+          <span
+            ><b>Bairro:</b>
+            {{ user.neighborhood ? user.neighborhood : '' }}</span
+          >
+          <br />
+          <span><b>Cep:</b> {{ user.cep ? user.cep : '' }}</span>
+          <br />
+        </v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn outlined rounded text @click="editUser"> Editar </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => ({}),
+  methods: {
+    editUser() {},
+  },
+}
+</script>
+
+<style>
+</style>
--- pages/usuarios.vue
+++ pages/usuarios.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>
-    <h1>Usuários</h1>
+  <div class="tw-m-8 tw-flex tw-flex-col">
+    <div
+      class="
+        tw-mt-4 tw-w-full tw-grid tw-gris-cols-1
+        lg:tw-grid-cols-3
+        tw-justify-items-center tw-gap-2
+      "
+    >
+      <div v-for="(user, index) in users" :key="index">
+        <list-users :user="user" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $axios, error }) {
+    let data = []
+    await $axios
+      .get('/api/public_html/api/user/')
+      .then((res) => {
+        data = res?.data?.data
+      })
+      .catch()
+    return { users: data }
+  },
+}
 </script>
 
 <style>
