477d0da53e43ac6f1c56266d4c780d373db45f0b
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2022-03-28 08:20:20 -0300

    lista de pedidos

---
+++ components/ListOrders.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-card class="mx-auto" max-width="400" outlined>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-subtitle>
+          <span
+            ><b>Total: </b>R$ {{ order.total ? order.total : '' }} reais</span
+          >
+          <br />
+          <span><b>Data:</b> {{ order.date ? order.date : '' }}</span>
+          <br />
+          <span><b>Hora:</b> {{ order.hour ? order.hour : '' }}</span>
+          <br />
+          <span
+            ><b>Produtos:</b> {{ order.products ? order.products : '' }}</span
+          >
+          <br />
+          <span
+            ><b>Nome:</b> {{ order.first_name ? order.first_name : '' }}</span
+          >
+          <br />
+          <span
+            ><b>Sobrenome:</b>
+            {{ order.last_name ? order.last_name : '' }}</span
+          >
+          <br />
+          <span><b>Telefone:</b> {{ order.phone ? order.phone : '' }}</span>
+          <br />
+          <span><b>Celular:</b> {{ order.cel ? order.cel : '' }}</span>
+          <br />
+          <span><b>Email:</b> {{ order.email ? order.email : '' }}</span>
+          <br />
+          <span><b>CPF:</b> {{ order.cpf ? order.cpf : '' }}</span>
+          <br />
+          <span><b>Rua:</b> {{ order.street ? order.street : '' }}</span>
+          <br />
+          <span><b>Número:</b> {{ order.number ? order.number : '' }}</span>
+          <br />
+          <span><b>Cidade:</b> {{ order.city ? order.city : '' }}</span>
+          <br />
+          <span><b>Estado:</b> {{ order.state ? order.state : '' }}</span>
+          <br />
+          <span><b>País:</b> {{ order.country ? order.country : '' }}</span>
+          <br />
+          <span
+            ><b>Bairro:</b>
+            {{ order.neighborhood ? order.neighborhood : '' }}</span
+          >
+          <br />
+          <span><b>Cep:</b> {{ order.cep ? order.cep : '' }}</span>
+          <br />
+        </v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    order: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => ({}),
+}
+</script>
+
+<style>
+</style>
--- pages/pedidos.vue
+++ pages/pedidos.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>
-    <h1>Pedidos</h1>
+  <div class="tw-m-8 tw-flex tw-flex-col">
+    <div
+      class="
+        tw-mt-4 tw-w-full tw-grid tw-gris-cols-1
+        lg:tw-grid-cols-3
+        tw-justify-items-center tw-gap-2
+      "
+    >
+      <div v-for="(order, index) in orders" :key="index">
+        <list-orders :order="order" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $axios, error }) {
+    let data = []
+    await $axios
+      .get('/api/public_html/api/order/')
+      .then((res) => {
+        data = res?.data?.data
+      })
+      .catch()
+    return { orders: data }
+  },
+}
 </script>
 
 <style>
